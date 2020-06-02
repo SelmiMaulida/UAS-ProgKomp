2491d3266f0137faf42041d0dca476c459e350b7
SelmiMaulida <65958427+SelmiMaulida@users.noreply.github.com>
2020-06-02 16:22:20 +0700

    menjelaskan program person.py

--- Person.py
+++ Person.py
@@ -6,13 +6,14 @@ class Person:
   _name_kategori_file = ""
   
   def __init__(self, country, person_name):
-      # create folder if not exist
+      # membuat folder melalui program
       if not os.path.exist(country):
           os.makedirs(country)
       self._person_name = country
       self._name_kategori_file = os.path.join(country, person_name + ".txt")
-      # create file first if not exist
-      __file = open(self._name_kategori_file, "a")
+      
+      # membuat file melalui program
+      __file = open(self._name_kategori_file, "a") # a untuk append data text
       __file.close()
     
     
@@ -36,6 +37,7 @@ class Person:
           print("Berhasil menambahkan data!")
           iterasi = True if input("ingin menambahkan barang lagi?(y/n)") == "y" else False
           
+   #fungsi untuk menambah produk       
    def add_product(self, product, amount):
        if product not in self.get_list_product():
             __file = open(self._name_kategori_file, "a")
@@ -43,11 +45,12 @@ class Person:
             __file.close()
        else:
             print("Produk telah ditambahkan")
-   
+          
+   #fungsi untuk menyimpan list produk
    def _get_list_product(self):
-       __file = open(self._name_kategori_file, "r")
+       __file = open(self._name_kategori_file, "r") # r untuk read list produk yang sudah ditambahkan
        list_beli = __file.readlines()
-       for index, item in enumerate(list_beli):
+       for index, item in enumerate(list_beli): #enumerate berfungsi untuk menambahkan penghitung dalam iterasi, kemudian dikembalikan dalam objek enumerasi yang dapat digunakan secara langsung untuk loop atau dikonversi menjadi daftar tuple melalui metode list
            list[index] = item.split(",")[0]
        return list_beli
             
