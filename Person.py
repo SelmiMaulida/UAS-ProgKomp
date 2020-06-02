fedac7c68fbe2a47d04849f9b19f1241cdc7f413
nindyas <66062576+nindyas@users.noreply.github.com>
2020-06-02 13:37:11 +0700

    Update Person.py

--- Person.py
+++ Person.py
@@ -1,1 +1,18 @@
+import os
 
+
+class Person:
+  _person_name = ""
+  _name_kategori_file = ""
+  
+  def __init__(self, country, person_name):
+      # create folder if not exist
+      if not os.path.exist(country):
+          os.makedirs(country)
+      self._person_name = country
+      self._name_kategori_file = os.path.join(country, person_name + ".txt")
+      # create file first if not exist
+      __file = open(self._name_kategori_file, "a")
+      __file.close()
+    
+    
