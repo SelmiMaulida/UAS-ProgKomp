856aa849399faba9c6e54c5ac05fa51837775bb7
NisaNurAzizah <66210054+NisaNurAzizah@users.noreply.github.com>
2020-06-04 22:16:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from os import system
+
 from MenuNegara import MenuNegara #import dari MenuNegara.py untuk diproses pada main menu
+from MenuPesanan import MenuPesanan
+
 
 class MainMenu :
   def switch(self, kode) : 
@@ -11,8 +15,8 @@ class MainMenu :
     print("\n\n")
     _iterasi = True
     menuNegara = MenuNegara()
-    
     while _iterasi :
+      system("cls")
       print("\tNegara JasTip")
       if len(menuNegara.listNegara()) != 0:
          print(menuNegara.listNegara())
@@ -23,8 +27,8 @@ class MainMenu :
       print("\t1. Tambah Negara\n"
             "\t2. Edit Barang Negara\n"
             "\t3. Hapus Negara")
-      menu_kode = int(input("Pilihan :"))
-      menuNegara.switch(menu_kode)
+      menu_code = int(input("Pilihan :"))
+      menuNegara.switch(menu_code)
       _iterasi = True if (input("do you want to quit ?(y/n)") == "n") else False
        
   def _menu_2(self) :
@@ -35,6 +39,7 @@ class MainMenu :
     else :
       iterasi = True
       while iterasi :
+        system("cls")
         print("\tJasa Titip Antar Negara")
         for index, country in enumerate(menuNegara.listNegara()):
           print(str(index + 1) + "." + country)
@@ -44,7 +49,12 @@ class MainMenu :
         print("Menu JasTip :")
         print("\t1. Tambah Pesanan")
         print("\t2. Hapus Pesanan")
-        
+        menu_code = int(input("Pilihan : "))
+        menu_pesanan = MenuPesanan(menuNegara.listNegara()[index_country])
+        menu_pesanan.switch(menu_code)
+        iterasi = True if (input("do you want to quit?(y/n) ") == "n") else False
+                                
+                                
 if __name__ == "__main__":
   menu = MainMenu()
   iterasi = True
